9c07d2843d9ee3dc5c8180a45ad41aeb7ef3e3d0
jerem <jerem@LAPTOP>
2021-11-06 19:56:19 +0100

    form and fetch data

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class WeatherComponent implements OnInit {
 
   WeatherData:any;
+  city:string ='london';
 
   @Input() isTemp: boolean = false;
 
@@ -19,8 +20,12 @@ export class WeatherComponent implements OnInit {
   }
 
   getWeatherData(){
-    let data = JSON.parse('{"coord":{"lon":-0.1257,"lat":51.5085},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}],"base":"stations","main":{"temp":285.03,"feels_like":284.36,"temp_min":284.09,"temp_max":285.68,"pressure":1021,"humidity":80},"visibility":10000,"wind":{"speed":6.76,"deg":240,"gust":15.1},"clouds":{"all":77},"dt":1636222982,"sys":{"type":2,"id":2019646,"country":"GB","sunrise":1636182179,"sunset":1636215918},"timezone":0,"id":2643743,"name":"London","cod":200}');
-    this.setWeatherData(data);
+    //recup donnée de l'API
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=edddbb4f6c19c4b4d525f69c2345d6e2`)
+    .then(response=>response.json())
+    .then(data=>{this.setWeatherData(data);})
+    // let data = JSON.parse('{"coord":{"lon":-0.1257,"lat":51.5085},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}],"base":"stations","main":{"temp":285.03,"feels_like":284.36,"temp_min":284.09,"temp_max":285.68,"pressure":1021,"humidity":80},"visibility":10000,"wind":{"speed":6.76,"deg":240,"gust":15.1},"clouds":{"all":77},"dt":1636222982,"sys":{"type":2,"id":2019646,"country":"GB","sunrise":1636182179,"sunset":1636215918},"timezone":0,"id":2643743,"name":"London","cod":200}');
+    // this.setWeatherData(data);
   }
 
   setWeatherData(data:any){
@@ -44,5 +49,14 @@ export class WeatherComponent implements OnInit {
     }else{
       this.isTemp = true;
     }
+  }
+
+  searchCity() {
+    this.WeatherData = {
+      main : {},
+      isDay: true
+    };
+    this.getWeatherData();
+    console.log(this.WeatherData);
   }
 }
